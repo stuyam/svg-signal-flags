9d8d10ef0d2c332cd0fa5b10c582960dcadcd311
Stuart Yamartino <stuart@yamartino.com>
2016-12-24 15:32:57 -0500

    Renamed folders

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,20 @@
 var gulp   = require('gulp');
 var inject = require('gulp-inject-string')
 
-var RED    = 'RED';
-var BLUE   = 'BLUE';
-var YELLOW = 'YELLOW';
-var WHITE  = 'WHITE';
-var BLACK  = 'BLACK';
-var BORDER = 'BORDER';
+var RED    = '#b30507';
+var BLUE   = '#053378';
+var YELLOW = '#fdd13d';
+var WHITE  = '#ffffff';
+var BLACK  = '#000000';
+var BORDER = '#d8d8d8';
 
 gulp.task('build', function() {
-  gulp.src('./svg/*.svg')
+  gulp.src('./unbuilt-svg/*.svg')
   .pipe(inject.replace('RED', RED))
   .pipe(inject.replace('BLUE', BLUE))
   .pipe(inject.replace('YELLOW', YELLOW))
   .pipe(inject.replace('WHITE', WHITE))
   .pipe(inject.replace('BLACK', BLACK))
   .pipe(inject.replace('BORDER', BORDER))
-  .pipe(gulp.dest('./dist'));
+  .pipe(gulp.dest('./svg'));
 });
