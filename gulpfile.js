9a60e9810e6147156a2c070ec03dd02f612f58cd
Stuart Yamartino <stuart@yamartino.com>
2016-12-28 15:27:04 -0500

    Build pngs from svg during build

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
-var gulp   = require('gulp');
-var inject = require('gulp-inject-string')
+var gulp    = require('gulp');
+var inject  = require('gulp-inject-string')
+var svg2png = require('gulp-svg2png');
 
 var RED    = '#b30507';
 var BLUE   = '#053378';
@@ -9,12 +10,14 @@ var BLACK  = '#000000';
 var BORDER = '#d8d8d8';
 
 gulp.task('build', function() {
-  gulp.src('./unbuilt-svg/*.svg')
+  gulp.src('./src/*.svg')
   .pipe(inject.replace('RED', RED))
   .pipe(inject.replace('BLUE', BLUE))
   .pipe(inject.replace('YELLOW', YELLOW))
   .pipe(inject.replace('WHITE', WHITE))
   .pipe(inject.replace('BLACK', BLACK))
   .pipe(inject.replace('BORDER', BORDER))
-  .pipe(gulp.dest('./svg'));
+  .pipe(gulp.dest('./svg'))
+  .pipe(svg2png())
+  .pipe(gulp.dest('./png'));
 });
